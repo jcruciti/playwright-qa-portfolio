fc687605d4be584dfdd48d444724d666a3dde791
Joe Cruciti <joede.cruciti@gmail.com>
2026-05-07 13:41:05 -0300

    Delete

--- utils/basicTest.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config({
-  path: require('path').resolve(__dirname, '../../.env'),
-});
-
-console.log('USER:', process.env.SAUCE_USER);
-console.log('PASSWORD:', process.env.SAUCE_PASSWORD);
