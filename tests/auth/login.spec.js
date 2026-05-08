ae793ae6299abb07138e4867eb2162908d22c80d
Joe Cruciti <joede.cruciti@gmail.com>
2026-05-08 19:27:17 -0300

    test.step() implementation on login.spec.js

--- tests/auth/login.spec.js
+++ tests/auth/login.spec.js
@@ -9,18 +9,30 @@ test.describe('Login', () => {
   });
 
   test('should login succesfully with valid credentials', async ({ page }) => {
-    await loginPage.open();
-    await loginPage.login(process.env.SAUCE_USER, process.env.SAUCE_PASSWORD);
+    await test.step('Open login page', async () => {
+      await loginPage.open();
+    });
 
-    await expect(page).toHaveURL(/inventory/);
+    await test.step('Login with valid credentials', async () => {
+      await loginPage.login(process.env.SAUCE_USER, process.env.SAUCE_PASSWORD);
+    });
+
+    await test.step('Validate successful login', async () => {
+      await expect(page).toHaveURL(/inventory/);
+    });
   });
 
-  test('should display error message using invalid credentials', async ({
-    page,
-  }) => {
-    await loginPage.open();
-    await loginPage.login('error_user', 'inv@l!d');
+  test('should display error message using invalid credentials', async () => {
+    await test.step('Open login page', async () => {
+      await loginPage.open();
+    });
 
-    await expect(loginPage.getErrorMessage()).toBeVisible();
+    await test.step('Login with invalid credentials', async () => {
+      await loginPage.login('error_user', 'inv@l!d');
+    });
+
+    await test.step('Validate login error message is displayed', async () => {
+      await expect(loginPage.getErrorMessage()).toBeVisible();
+    });
   });
 });
